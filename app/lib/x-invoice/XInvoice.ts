95bd68f2f5f19932e6286e3b4ac075fcd06e6688
Tim Bastin <bastin.tim@gmail.com>
2020-11-02 17:39:29 +0100

    Added InvoiceLine types

--- app/lib/x-invoice/XInvoice.ts
+++ app/lib/x-invoice/XInvoice.ts
@@ -2,11 +2,7 @@ import XInvoiceEnumTag from './XInvoiceEnumTag';
 import XInvoiceStringTag from './XInvoiceStringTag';
 import XInvoiceNumberTag from './XInvoiceNumberTag';
 import XInvoiceDateTag from './XInvoiceDateTag';
-
-export interface TagWithAttributes {
-  attributes?: Record<string, string>[];
-  content: string;
-}
+import XInvoiceTag from './XInvoiceTag';
 
 export interface InvoicePeriod {}
 
@@ -37,7 +33,87 @@ export interface PaymentTerms {}
 export interface AllowanceCharge {}
 export interface TaxTotal {}
 export interface LegalMonetaryTotal {}
-export interface InvoiceLine {}
+export interface InvoicePeriod {
+  StartDate: XInvoiceDateTag;
+  EndDate: XInvoiceDateTag;
+}
+export interface OrderLineReference {
+  LineId: XInvoiceNumberTag;
+}
+
+export interface DocumentReference {
+  ID: XInvoiceStringTag;
+  DocumentTypeCode: 130;
+}
+export enum Bool {
+  true,
+  false,
+}
+export interface Identification {
+  ID: XInvoiceNumberTag;
+}
+export enum IdentificationCode {
+  CN,
+}
+export interface OriginCountry {
+  IdentificationCode: XInvoiceEnumTag<IdentificationCode>;
+}
+export interface CommodityClassification {
+  ItemClassificationCode: XInvoiceNumberTag;
+}
+
+export enum TaxSchemeID {
+  VAT,
+}
+export interface TaxScheme {
+  ID: XInvoiceEnumTag<TaxSchemeID>;
+}
+export interface ClassifiedTaxCategory extends Identification {
+  Percent?: XInvoiceNumberTag;
+  TaxScheme: TaxScheme;
+}
+export interface AdditionalItemProperty {
+  Name: XInvoiceTag<any>;
+  Value: XInvoiceTag<any>;
+}
+export interface Item {
+  Description?: XInvoiceStringTag;
+  Name: XInvoiceStringTag;
+  BuyersItemIdentification?: Identification;
+  SellersItemIdentification?: Identification;
+  StandardItemIdentification?: Identification;
+  OriginCountry?: OriginCountry;
+  CommodityClassification?: CommodityClassification;
+  ClassifiedTaxCategory: ClassifiedTaxCategory;
+  AdditionalItemProperty?: AdditionalItemProperty[];
+}
+export interface AllowanceCharge {
+  ChargeIndicator: XInvoiceEnumTag<Bool>;
+  AllowanceChargeReasonCode?: XInvoiceNumberTag;
+  AllowanceChargeReason?: XInvoiceStringTag;
+  MultiplierFactorNumeric: XInvoiceNumberTag;
+  Amount: XInvoiceNumberTag;
+  BaseAmount?: XInvoiceNumberTag;
+  Item: Item;
+}
+export interface Price {
+  PriceAmount: XInvoiceNumberTag;
+  BaseQuantity?: XInvoiceNumberTag;
+  AllowanceCharge?: AllowanceCharge;
+}
+export interface InvoiceLine {
+  ID: XInvoiceNumberTag;
+  Note?: XInvoiceStringTag;
+  InvoicedQuantity: XInvoiceNumberTag;
+  LineExtensionAmount: XInvoiceNumberTag;
+  AccountingCost?: XInvoiceStringTag;
+  InvoicePeriod?: InvoicePeriod;
+  OrderLineReference?: OrderLineReference;
+  DocumentReference?: DocumentReference;
+  AllowanceCharge?: AllowanceCharge;
+  Item: Item;
+  Price: Price;
+}
 
 export enum CurrencyCodes {
   EUR,
@@ -61,4 +137,5 @@ export interface XInvoice {
   InvoicePeriod?: InvoicePeriod;
   BillingsReference?: BillingReference[];
   TaxTotal: TaxTotal | [TaxTotal, TaxTotal];
+  InvoiceLine: InvoiceLine[];
 }
