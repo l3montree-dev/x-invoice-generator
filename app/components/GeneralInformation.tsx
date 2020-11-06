20315ed85e42a143e7a1c9872c7f6eb08d44b8f3
Tim Bastin <bastin.tim@gmail.com>
2020-11-06 19:56:55 +0100

    Finished general information

--- app/components/GeneralInformation.tsx
+++ app/components/GeneralInformation.tsx
@@ -16,19 +16,32 @@ const GeneralInformation = () => {
           </Form.Item>
         </Col>
         <Col span={8}>
-          <Form.Item required name="BuyerReference" label="Leitweg-ID">
+          <Form.Item
+            tooltip="Ein vom Erwerber zugewiesener und für interne Lenkungszwecke benutzter Bezeichner."
+            required
+            name="BuyerReference"
+            label="Leitweg-ID"
+          >
             <Input placeholder="1234567" />
           </Form.Item>
         </Col>
         <Col span={8}>
           <Form.Item required name="IssueDate" label="Rechnungsdatum">
-            <DatePicker style={style.datePicker} picker="date" />
+            <DatePicker
+              placeholder="Datum auswählen"
+              style={style.datePicker}
+              picker="date"
+            />
           </Form.Item>
         </Col>
       </Row>
       <Row gutter={16}>
         <Col span={8}>
-          <Form.Item required name="OrderReference.ID" label="Bestellnummer">
+          <Form.Item
+            tooltip="Eine vom Erwerber ausgegebene Kennung für eine referenzierte Bestellung."
+            name="OrderReference.ID"
+            label="Bestellnummer"
+          >
             <Input placeholder="1234567" />
           </Form.Item>
         </Col>
@@ -39,26 +52,46 @@ const GeneralInformation = () => {
         </Col>
         <Col span={8}>
           <Form.Item required name="DueDate" label="Fälligkeitsdatum">
-            <DatePicker style={style.datePicker} picker="date" />
+            <DatePicker
+              placeholder="Datum auswählen"
+              style={style.datePicker}
+              picker="date"
+            />
           </Form.Item>
         </Col>
       </Row>
       <Row gutter={16}>
         <Col span={12}>
-          <Form.Item name="StartDate" label="Abrechnungszeitraum vom">
-            <DatePicker style={style.datePicker} />
+          <Form.Item
+            required
+            tooltip="Das Datum, an dem der Rechnungszeitraum beginnt."
+            name="StartDate"
+            label="Abrechnungszeitraum vom"
+          >
+            <DatePicker
+              placeholder="Datum auswählen"
+              style={style.datePicker}
+            />
           </Form.Item>
         </Col>
         <Col span={12}>
-          <Form.Item name="EndDate" label="Abrechnungszeitraum bis">
-            <DatePicker style={style.datePicker} />
+          <Form.Item
+            required
+            tooltip="Das Datum, an dem der Rechnungszeitraum endet."
+            name="EndDate"
+            label="Abrechnungszeitraum bis"
+          >
+            <DatePicker
+              placeholder="Datum auswählen"
+              style={style.datePicker}
+            />
           </Form.Item>
         </Col>
       </Row>
       <Row gutter={16}>
         <Col span={24}>
           <Form.Item name="Note" label="Bemerkung">
-            <Input placeholder="1234567" />
+            <Input />
           </Form.Item>
         </Col>
       </Row>
