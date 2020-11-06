cbb897989744e97a6d72b17bd53c75c51aac9cbc
Tim Bastin <bastin.tim@gmail.com>
2020-11-06 20:36:13 +0100

    Finished Totals

--- app/components/Totals.tsx
+++ app/components/Totals.tsx
@@ -7,49 +7,41 @@ const Totals = () => {
       <Row gutter={16}>
         <Col span={8}>
           <Form.Item
-            required
             name="LegalMonetaryTotal.LineExtensionAmount"
             label="Summe aller Positionen netto"
           >
-            <Input placeholder="1234567" />
+            <Input disabled />
           </Form.Item>
         </Col>
         <Col span={8}>
           <Form.Item
-            required
             name="LegalMonetaryTotal.TaxExclusiveAmount"
             label="Gesamtsumme netto"
           >
-            <Input placeholder="1234567" />
+            <Input disabled />
           </Form.Item>
         </Col>
         <Col span={8}>
-          <Form.Item
-            required
-            name="TaxTotal.TaxAmount"
-            label="Summe Umsatzsteuer"
-          >
-            <Input placeholder="1234567" />
+          <Form.Item name="TaxTotal.TaxAmount" label="Summe Umsatzsteuer">
+            <Input disabled />
           </Form.Item>
         </Col>
       </Row>
       <Row gutter={16}>
         <Col span={12}>
           <Form.Item
-            required
             name="LegalMonetaryTotal.TaxInclusiveAmount"
             label="Gesamtsumme brutto"
           >
-            <Input placeholder="1234567" />
+            <Input disabled />
           </Form.Item>
         </Col>
         <Col span={12}>
           <Form.Item
-            required
             name="LegalMonetaryTotal.PayableAmount"
             label="Fälliger Betrag"
           >
-            <Input placeholder="1234567" />
+            <Input disabled />
           </Form.Item>
         </Col>
       </Row>
