1ff42fc825d22654752255f809979da0ad19e5f8
Tim Bastin <bastin.tim@gmail.com>
2020-11-07 09:19:45 +0100

    Added persistent storage service class

--- app/features/import/Import.tsx
+++ app/features/import/Import.tsx
@@ -1,7 +1,33 @@
 import React from 'react';
+import { InboxOutlined } from '@ant-design/icons';
+import { Upload } from 'antd';
+import { DraggerProps } from 'antd/lib/upload/Dragger';
+import { RcCustomRequestOptions } from 'antd/es/upload/interface';
+
+const props: DraggerProps = {
+  name: 'file',
+  multiple: false,
+  // accept: 'text/xml',
+  action: 'https://www.mocky.io/v2/5cc8019d300000980a055e76',
+  customRequest(info: RcCustomRequestOptions) {
+    console.log(info);
+  },
+};
 
 const Import = () => {
-  return <div>Import</div>;
+  return (
+    <div>
+      <Upload.Dragger {...props}>
+        <p className="ant-upload-drag-icon">
+          <InboxOutlined />
+        </p>
+        <p className="ant-upload-text">
+          Ziehe eine XML Datei in diesen Bereich oder klicke, um die Datei
+          auszuwählen
+        </p>
+      </Upload.Dragger>
+    </div>
+  );
 };
 
 export default Import;
---
+++ app/services/PersistentStorage.ts
@@ -0,0 +1,55 @@
+import { readFileSync, writeFileSync } from 'fs';
+import * as electron from 'electron';
+import { join } from 'path';
+
+interface PersistentStore {}
+/**
+ * @Singleton
+ */
+class PersistentStorage {
+  public getInstance(): PersistentStorage {
+    if (this.instance) {
+      return this.instance;
+    }
+    return new PersistentStorage();
+  }
+
+  get(key: keyof PersistentStore) {
+    return this.store[key];
+  }
+
+  set<Key extends keyof PersistentStore>(key: Key, val: PersistentStore[Key]) {
+    this.store[key] = val;
+    writeFileSync(this.path, JSON.stringify(this.store));
+  }
+
+  private static DEFAULTS: PersistentStore = {};
+
+  private instance?: PersistentStorage;
+
+  private readonly path: string;
+
+  private readonly store: PersistentStore;
+
+  private constructor() {
+    // Renderer process has to get `app` module via `remote`, whereas the main process can get it directly
+    // app.getPath('userData') will return a string of the user's app data directory path.
+    const userDataPath = (electron.app || electron.remote.app).getPath(
+      'userData'
+    );
+    this.path = join(userDataPath, 'store.json');
+
+    this.store = this.parseFile();
+  }
+
+  private parseFile(): PersistentStore {
+    // We'll try/catch it in case the file doesn't exist yet, which will be the case on the first application run.
+    // `fs.readFileSync` will return a JSON string which we then parse into a Javascript object
+    try {
+      return JSON.parse(readFileSync(this.path).toString());
+    } catch (error) {
+      // if there was some kind of error, return the passed in defaults instead.
+      return PersistentStorage.DEFAULTS;
+    }
+  }
+}
